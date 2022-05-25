6314dcbfa58a7b3499cbe4770d6715a5f3e5ad8d
Emircyn <emircan.erdemci@hotmail.com>
2022-05-25 23:07:21 +0300

    -u

--- src/App.vue
+++ src/App.vue
@@ -3,21 +3,17 @@ import { onMounted, ref } from 'vue';
 import { RouterLink, RouterView } from 'vue-router';
 import FooterVue from './components/Footer.vue';
 import HeaderVue from './components/Header.vue';
-import noInternetVue from './components/noInternet.vue';
 import { UseMainStore } from './stores/mainStore';
-console.log(navigator.onLine);
-const internet = ref(true);
+
 const mainStore = UseMainStore();
 onMounted(() => {
-  internet.value = navigator.online;
   mainStore.fetchMovies();
 });
 </script>
 
 <template>
   <HeaderVue />
-  <noInternetVue v-if="internet" />
-  <RouterView v-else />
+  <RouterView />
   <FooterVue />
 </template>
 
